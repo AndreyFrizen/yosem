6e097bbdd2d820f3c4c3d96298b7d9059d879804
Andrey A.A.. Frizen <frizenandrey@bkgau.ru>
2026-02-06 10:58:39 +0300

    v1

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -1,7 +1,11 @@
 package handler
 
 import (
+	"net/http"
+	"yosem/internal/models"
 	"yosem/internal/service"
+
+	"github.com/labstack/echo/v5"
 )
 
 type Handler struct {
@@ -17,23 +21,46 @@ func NewService(s *service.Service) *Handler {
 // goods
 
 // get goods service
-func (h *Handler) Goods() error {
-	return nil
+func (h *Handler) Goods(c *echo.Context) error {
+	if err := h.Service.Goods(); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, nil)
 }
 
 // add goods on storage
-func (h *Handler) AddGoods() error {
-	return nil
+func (h *Handler) AddGoods(c *echo.Context) error {
+	model := new(models.Goods)
+	if err := c.Bind(model); err != nil {
+		return err
+	}
+
+	if err := h.Service.AddGoods(model); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, model)
 }
 
 // update goods on storage
-func (h *Handler) UpdateGoods() error {
-	return nil
+func (h *Handler) UpdateGoods(c *echo.Context) error {
+	model := new(models.Goods)
+	if err := c.Bind(model); err != nil {
+		return err
+	}
+
+	if err := h.Service.AddGoods(model); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, model)
 }
 
 // delete goods from storage
-func (h *Handler) DeleteGoods() error {
-	return nil
+func (h *Handler) DeleteGoods(c *echo.Context) error {
+	id := c.Param("id")
+	if err := h.Service.DeleteGoods(id); err != nil {
+		return err
+	}
+	return c.NoContent(http.StatusNoContent)
 }
 
 // users
